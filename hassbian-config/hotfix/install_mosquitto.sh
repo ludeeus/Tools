6e08210f0cabdc448d9c8ad673b79a7af42a836c
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-01-14 18:35:56 +0100

    Update install_mosquitto.sh

--- hassbian-config/hotfix/install_mosquitto.sh
+++ hassbian-config/hotfix/install_mosquitto.sh
@@ -1,68 +1,99 @@
 #!/bin/bash
-# Date: 12/29/2017
+# Date: 12/29/2017 -- Issue resolved 01/14/18
 # This is an HotFix for issues with hassbian-config install mosquitto.
 # This fix can be implemented by running this:
 # sudo curl -o /opt/hassbian/suites/install_mosquitto.sh https://raw.githubusercontent.com/ludeeus/Tools/master/hassbian-config/hotfix/install_mosquitto.sh && sudo hassbian-config install mosquitto
 
+#!/bin/bash
+
 function mosquitto-show-short-info {
-    echo "Mosquitto Installer for Hassbian."
+    echo "Mosquitto Installer for Hassbian"
 }
 
 function mosquitto-show-long-info {
-	echo "Installs the Mosquitto package for setting up a local MQTT server."
+	echo "Installs the Mosquitto package for setting up a local MQTT server"
 }
 
 function mosquitto-show-copyright-info {
-    echo "Install script by Ludeeus <https://github.com/ludeeus>"
+    echo "Copyright(c) 2016 Dale Higgs <https://github.com/dale3h>"
+    echo "Modified by Landrash for use with Hassbian."
 }
 
 function mosquitto-install-package {
 mosquitto-show-short-info
 mosquitto-show-copyright-info
-echo
-echo "This should only be run if there is an issue with the installer in the image."
-echo "Cleaning up previous tries of installing."
-sudo rm /etc/apt/sources.list.d/mosquitto-stretch.list*
-sudo rm -R /etc/mosquitto
-sudo apt remove -y mosquitto mosquitto-clients libmosquitto1 libc-ares2 libssl1.0.0
 
-echo
-echo
-echo "Downloading mosquitto and required dependencies..."
-sudo mkdir /tmp/mosquitto_install
-cd /tmp/mosquitto_install
-sudo wget -q http://ftp.se.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb
-sudo wget -q http://ftp.se.debian.org/debian/pool/main/c/c-ares/libc-ares2_1.12.0-1+deb9u1_armhf.deb
-sudo wget -q http://repo.mosquitto.org/debian/pool/main/m/mosquitto/mosquitto_1.4.14-0mosquitto1~nows1_armhf.deb
-sudo wget -q http://repo.mosquitto.org/debian/pool/main/m/mosquitto/libmosquitto1_1.4.14-0mosquitto1~nows1_armhf.deb
-sudo wget -q http://repo.mosquitto.org/debian/pool/main/m/mosquitto/mosquitto-clients_1.4.14-0mosquitto1~nows1_armhf.deb
+echo -e "\e[31mThis workaround is no longer needed..."
+echo -e "\e[31mThis issue was resolved in the main repo 01/14/18..."
+echo -e "\e[31mRedirecting this installer to use the one in hassbian-config..."
 
-echo "Installing mosquitto and required dependencies..."
-sudo dpkg -i *
-echo
-echo "Cleanup..."
-cd
-sudo rm -R /tmp/mosquitto_install
+if [ "$(id -u)" != "0" ]; then
+echo "This script must be run with sudo. Use \"sudo ${0} ${*}\"" 1>&2
+return 1
+fi
 
-echo
-echo
-echo "Writing default configuration..."
+echo "Adding mosquitto user"
+adduser mosquitto --system --group
+
+echo "Creating pid file"
+touch /var/run/mosquitto.pid
+chown mosquitto:mosquitto /var/run/mosquitto.pid
+
+echo "Creating data directory"
+mkdir -p /var/lib/mosquitto
+chown mosquitto:mosquitto /var/lib/mosquitto
+
+echo "Installing repository key"
+wget -O - http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key | apt-key add -
+
+echo "Adding repository"
+cd /etc/apt/sources.list.d
+wget http://repo.mosquitto.org/debian/mosquitto-stretch.list
+
+echo "Installing mosquitto"
+apt-get update
+apt install -y mosquitto mosquitto-clients
+
+
+if [[ $? > 0 ]]
+then
+   echo "First try failed, adding dependencies and trying again."
+   echo "This is an workaround and will be omited once it's fixed upstream."
+   echo "Downloading dependencies"
+   cd
+   wget http://ftp.se.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb
+   wget http://ftp.se.debian.org/debian/pool/main/libw/libwebsockets/libwebsockets3_1.2.2-1_armhf.deb
+
+   echo "Installing dependencies"
+   sudo dpkg -i libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb
+   sudo dpkg -i libwebsockets3_1.2.2-1_armhf.deb
+
+   echo "Cleanup dependencies"
+   rm libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb
+   rm libwebsockets3_1.2.2-1_armhf.deb
+   
+   echo "Retrying installation of mosquitto"
+   apt install -y mosquitto mosquitto-clients
+else
+    echo ""
+fi
+
+echo "Writing default configuration"
 cd /etc/mosquitto
 mv mosquitto.conf mosquitto.conf.backup
 cp /opt/hassbian/suites/files/mosquitto.conf /etc/mosquitto/mosquitto.conf
+chown mosquitto:mosquitto mosquitto.conf
 
-echo
-echo
 echo "Initializing password file"
 touch pwfile
 chown mosquitto:mosquitto pwfile
 chmod 0600 pwfile
 
 echo
-echo
 echo "Please take a moment to setup your first MQTT user"
-echo "If no choice is made a default account will be created (pi/raspberry)"
+echo "If no choice is made a default account will be created"
 echo
+
 echo -n "Username: "
 read mqtt_username
 if [ ! "$mqtt_username" ]; then
@@ -76,25 +107,27 @@ if [ ! "$mqtt_password" ]; then
   mqtt_password=raspberry
 fi
 
-echo "Creating password entry for user $mqtt_username..."
+echo "Creating password entry for user $mqtt_username"
 mosquitto_passwd -b pwfile "$mqtt_username" "$mqtt_password"
 
-echo "Starting Mosquitto..."
-(crontab -l ; echo "@reboot /usr/sbin/mosquitto -d")| crontab -
-mosquitto -d
+echo "Restarting Mosquitto service"
+systemctl enable mosquitto.service
+systemctl restart mosquitto.service
 
-ip_address=$(ifconfig | grep "inet.*broadcast" | grep -v 0.0.0.0 | awk '{print $2}')
+
+ip_address=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
 
 echo
 echo "Installation done!"
 echo
-echo "Your MQTT broker is running at $ip_address:1883 or if prefered hassbian.local:1883"
+echo "Your MQTT broker is running at $ip_address:1883 or if prefered hassbian.local"
 echo
 echo "To continue have a look at https://home-assistant.io/docs/mqtt/"
+echo
 echo "If you have issues with this script, please say something in the #Hassbian channel on Discord."
+echo "Original script by @dale3h"
 echo
 return 0
 }
 
-# Make this script function as it always has if run standalone, rather than issue a warning and do nothing.
-[[ $0 == "$BASH_SOURCE" ]] && webterminal-install-package
+[[ $_ == $0 ]] && echo "hassbian-config helper script; do not run directly, use hassbian-config install instead"
