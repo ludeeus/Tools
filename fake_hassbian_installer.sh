c7ec2ef5b8cdbc2aedd20878dbd3785ad13a498d
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-01-24 19:55:17 +0100

    Update fake_hassbian_installer.sh

--- fake_hassbian_installer.sh
+++ fake_hassbian_installer.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+#Run this comand to install:
+# $ wget -O - https://raw.githubusercontent.com/ludeeus/tools/master/fake_hassbian_installer.sh | sudo bash -
+#
 if [ "$(id -u)" != "0" ]; then
 echo "This script must be run with sudo. Use \"sudo ${0} ${*}\"" 1>&2
 exit 1
@@ -14,11 +18,12 @@ apt-get install -y git python3 curl python3-venv python3-pip bluetooth libblueto
 pip3 install --upgrade virtualenv
 
 echo "Adding homeassistant user and install dir..."
-####addgroup homeassistant
-####useradd -u 1001 -g homeassistant -rm homeassistant
-####mkdir /srv/homeassistant
-####chown homeassistant:homeassistant -R /srv/homeassistant
-####usermod -G dialout,i2c,gpio,spi -a homeassistant
+addgroup homeassistant
+useradd -u 1001 -g homeassistant -rm homeassistant
+mkdir /srv/homeassistant
+chown homeassistant:homeassistant -R /srv/homeassistant
+chown homeassistant:homeassistant -R /home/homeassistant
+usermod -G dialout,i2c,gpio,spi -a homeassistant
 
 
 echo "Downloading and installing hassbian-config..."
