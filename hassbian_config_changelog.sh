2ad63018da12100c1ae4f6bc7812915006d5d747
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-02-04 21:07:19 +0100

    Update hassbian_config_changelog.sh

--- hassbian_config_changelog.sh
+++ hassbian_config_changelog.sh
@@ -3,7 +3,7 @@
 
 read -p "Enter the version number: " VERSION
 echo "Verifying milestones..."
-MILESTONES=$(curl https://api.github.com/search/issues\?q\=milestone:v$VERSION+$
+MILESTONES=$(curl https://api.github.com/search/issues\?q\=milestone:v$VERSION+type:pr+repo:home-assistant/hassbian-scripts | jq -r '.items[]| "[#" +(.number|tostring) +"](" + .html_url + ") " + .title + " [@" + (.user | .login + "](" + .html_url + ")  ")')
 if [ ! "$MILESTONES" ]; then
         echo "No milestones... aborting..."
         exit
