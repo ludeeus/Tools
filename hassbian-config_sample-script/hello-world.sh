4c6d2169330ae76686a18bc7ceaa18d543a897f0
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-03-06 10:12:42 +0100

    Update hello-world.sh

--- hassbian-config_sample-script/hello-world.sh
+++ hassbian-config_sample-script/hello-world.sh
@@ -9,11 +9,11 @@ function hello-world-show-copyright-info {
   echo "Concept by Ludeeus <https://github.com/ludeeus>."
 }
 function hello-world-install-package {
-printf  "Hello world!\\n\\n"
-return 0
+  printf  "Hello world!\\n\\n"
+  return 0
 }
 function hello-world-upgrade-package {
-printf "Hello World!\\n\\n"
-return 0
+  printf "Hello World!\\n\\n"
+  return 0
 }
 [[ "$_" == "$0" ]] && echo "hassbian-config helper script; do not run directly, use hassbian-config instead"
